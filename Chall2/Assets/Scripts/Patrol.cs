087ff1c11eb80fb92809a0ea5e4a9e494b32653b
jmbyam <jmbyam@alaska.edu>
2020-04-06 11:55:32 -0800

    Make the snake a roming AI that will follow you

--- Chall2/Assets/Scripts/Patrol.cs
+++ Chall2/Assets/Scripts/Patrol.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
     public float speed;
+    public float stopDistance;
+    public float awarenessDistance;
     private float waitTime;
     public float startWaitTime;
 
+    public Transform target;
     public Transform[] moveSpots;
 
     private int randomSpot;
@@ -15,11 +18,21 @@ public class Patrol : MonoBehaviour
     void Start(){
       waitTime = startWaitTime;
       randomSpot = Random.Range(0,moveSpots.Length);
+      target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update(){
 
-      transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+      if(Vector3.Distance(transform.position, target.position) > awarenessDistance){
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+      } else {
+        if(Vector3.Distance(transform.position, target.position) > stopDistance){
+          transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+          Vector3 pos = transform.position;
+          pos.y = 0.25f;
+          transform.position = pos;
+        }
+      }
 
       if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
         if(waitTime <= 0){
